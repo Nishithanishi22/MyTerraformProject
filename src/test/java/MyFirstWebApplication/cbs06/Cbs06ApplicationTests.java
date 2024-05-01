f8e00dbccc4963a23bcf40a8be76a06d8c1c6f79
nishitha1306 <nishithanishi1306@gmail.com>
2024-04-30 23:50:39 -0400

    "committing the springboot project layers"

---
+++ src/test/java/MyFirstWebApplication/cbs06/Cbs06ApplicationTests.java
@@ -0,0 +1,13 @@
+package MyFirstWebApplication.cbs06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cbs06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
