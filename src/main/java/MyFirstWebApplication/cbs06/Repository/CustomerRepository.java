85ed8ca11bcf601a30804ae3b5548ef372f32a94
Nishithanishi22 <167028936+Nishithanishi22@users.noreply.github.com>
2024-06-13 20:08:04 -0400

    Delete src directory

--- src/main/java/MyFirstWebApplication/cbs06/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package MyFirstWebApplication.cbs06.Repository;
-
-import MyFirstWebApplication.cbs06.Entity.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long>
-{
-
-}
